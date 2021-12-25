b356e7b99b5f81aa3176993574028cf22dca9a68
Hoyong Jin <jin8495@gmail.com>
2021-12-25 17:03:00 +0900

    seems work fine

--- src/compressor/VPC.cpp
+++ src/compressor/VPC.cpp
@@ -99,6 +99,12 @@ unsigned VPC::compressLineAllWordSame(std::vector<uint8_t> &dataLine)
     // update compression stat
     static_cast<VPCResult*>(m_Stat)->Update(uncompressedLineSize, compressedLineSize, chosenCompModule);
 
+    // update residue stat
+    PredCompModule *predCompModule = static_cast<PredCompModule*>(m_CompModules[chosenCompModule]);
+    double mae = predCompModule->GetMAE(dataLine);
+    double mse = predCompModule->GetMSE(dataLine);
+    static_cast<VPCResult*>(m_Stat)->UpdateResidueStat(mae, mse);
+
     return compressedLineSize;
   }
 }
